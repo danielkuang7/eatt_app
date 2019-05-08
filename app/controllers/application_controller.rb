748b4785f0366b83152669d3ff7159614136cd80
Daniel Kuang <danielkuang@Daniels-MacBook-Pro-4.local>
2019-05-08 11:03:07 -0400

    updating heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
 
+    
     def authorize_request
         header = request.headers['Authorization']
         header = header.split(' ').last if header
